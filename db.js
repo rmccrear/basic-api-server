0f0121e4d81596f37774fe155b4809d40f56166a
Robert Mccreary <fooyinfighter@gmail.com>
2022-09-22 11:37:56 -0700

    add routes for farms

--- db.js
+++ db.js
@@ -2,12 +2,17 @@ const { Sequelize } = require("sequelize");
 
 let sequelize = null;
 if (process.env.NODE_ENV === "test") {
-  sequelize = new Sequelize("sqlite::memory:");
+  sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: ":memory:",
+    logging: false,
+  });
 } else {
   sequelize = new Sequelize({
     dialect: "sqlite",
-    storage: "/database.sqlite",
+    storage: "./database.sqlite",
   });
+  sequelize.sync();
 }
 
 module.exports = sequelize;
